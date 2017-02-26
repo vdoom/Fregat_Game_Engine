8a98238f2b41dca471358bd6c8814e8ece6c22be
Unknown <kuksynok.v@gmail.com>
2017-02-26 18:43:45 +0200

    Add GNU g++ support

--- Sources/Core/Math/MatOperations.h
+++ Sources/Core/Math/MatOperations.h
@@ -5,21 +5,21 @@
 #include "Vec3.h"
 #include "Vec2.h"
 
-namespace Fregat 
+namespace Fregat
 {
 	namespace Math
-	{	
+	{
 		//-----------------------------------------------------------------------------------
 
 		inline const Vec3 ToEulerMat4(const Mat4& t_mat)
 		{
 			Vec3 angle;
 			float x, y, C;
-		
+
 			angle.y = asinf(t_mat[2]);
 			C       = cosf(angle.y);
 			angle.y *= math_degrees;
-		
+
 			if (fabsf(C) > math_epsilon)
 			{
 				x       =  t_mat[10] / C;
@@ -35,11 +35,11 @@ namespace Fregat
 				y       =  t_mat[ 4];
 				angle.z = atan2f(y, x) * math_degrees;
 			}
-		
+
 			if (angle.x < 0) angle.x += 360;
 			if (angle.y < 0) angle.y += 360;
 			if (angle.z < 0) angle.z += 360;
-		
+
 			return angle;
 		}
 
@@ -62,16 +62,16 @@ namespace Fregat
 		{
 			return Mat4::Translation(t_vec.x, t_vec.y, t_vec.z);
 		}
-		
+
 		inline const Vec3 ToEulerMat3(const Mat3& t_mat)
 		{
 			Vec3 angle;
 			float x, y, C;
-		
+
 			angle.y = asinf(t_mat[2]);
 			C       = cosf(angle.y);
 			angle.y *= math_degrees;
-		
+
 			if (fabsf(C) > math_epsilon)
 			{
 				x       =  t_mat[8] / C;
@@ -87,24 +87,24 @@ namespace Fregat
 				y       =  t_mat[ 3];
 				angle.z = atan2f(y, x) * math_degrees;
 			}
-		
+
 			if (angle.x < 0) angle.x += 360;
 			if (angle.y < 0) angle.y += 360;
 			if (angle.z < 0) angle.z += 360;
-		
+
 			return angle;
 		}
-		
+
 		inline const Mat3 FromEulerMat3(const Vec3& t_vec)
 		{
 			return Mat3::FromEuler(t_vec.x, t_vec.y, t_vec.z);
 		}
-		
+
 		inline const Mat3 ScaleMat3(const Vec3& t_vec)
 		{
 			return Mat3::Scale(t_vec.x, t_vec.y, t_vec.z);
 		}
-		
+
 		inline const Mat3 TranslationMat3(const Vec3& t_vec)
 		{
 			return Mat3::Translation(t_vec.x, t_vec.y, t_vec.z);
@@ -115,7 +115,7 @@ namespace Fregat
 			Quat tmp;
 			const float t = t_mat[0] + t_mat[4] + t_mat[8];
 			float       s;
-		
+
 			if (t > 0.0f)
 			{
 				s = 0.5f / sqrtf(t + 1.0f);
@@ -148,13 +148,13 @@ namespace Fregat
 
 			return tmp;
 		}
-		
+
 		inline Quat Mat4ToQuat(const Mat4 &t_mat)
 		{
 			Quat tmp;
 			const float t = t_mat[0] + t_mat[5] + t_mat[10];
 			float       s;
-		
+
 			if (t > 0.0f)
 			{
 				s = 0.5f / sqrtf(t + 1.0f);
@@ -187,33 +187,33 @@ namespace Fregat
 
 			return tmp;
 		}
-		
+
 		inline float DeterminantMat4(const Mat4& t_mat)
 		{
 			float A, B, C, D, E, F;
-		
+
 			A = t_mat[10] * t_mat[15] - t_mat[11] * t_mat[14];
 			B = t_mat[ 9] * t_mat[15] - t_mat[11] * t_mat[13];
 			C = t_mat[ 9] * t_mat[14] - t_mat[10] * t_mat[13];
 			D = t_mat[ 8] * t_mat[15] - t_mat[11] * t_mat[12];
 			E = t_mat[ 8] * t_mat[14] - t_mat[10] * t_mat[12];
 			F = t_mat[ 8] * t_mat[13] - t_mat[ 9] * t_mat[12];
-		
+
 			return t_mat[0] * (A * t_mat[5] - B * t_mat[6] + C * t_mat[7])
 			     - t_mat[1] * (A * t_mat[4] - D * t_mat[6] + E * t_mat[7])
 			     + t_mat[2] * (B * t_mat[4] - D * t_mat[5] + F * t_mat[7])
 			     - t_mat[3] * (C * t_mat[4] - E * t_mat[5] + F * t_mat[6]);
 		}
-		
+
 		inline const Mat4 InverseMat4(const Mat4& t_mat)
 		{
 			const float det = 1.0f / DeterminantMat4(t_mat);
-		
+
 			#define MDET3(a0,a1,a2,a3,a4,a5,a6,a7,a8) \
 			            ( a0 * (a4 * a8 - a5 * a7) \
 			            - a1 * (a3 * a8 - a5 * a6) \
 			            + a2 * (a3 * a7 - a4 * a6) )
-		
+
 			// ~ 160 mul & 80 add
 			return Mat4(
 				 MDET3(t_mat[5], t_mat[6], t_mat[7], t_mat[9], t_mat[10], t_mat[11], t_mat[13], t_mat[14], t_mat[15]) * det,
@@ -232,10 +232,10 @@ namespace Fregat
 				 MDET3(t_mat[0], t_mat[1], t_mat[2], t_mat[8], t_mat[ 9], t_mat[10], t_mat[12], t_mat[13], t_mat[14]) * det,
 				-MDET3(t_mat[0], t_mat[1], t_mat[2], t_mat[4], t_mat[ 5], t_mat[ 6], t_mat[12], t_mat[13], t_mat[14]) * det,
 				 MDET3(t_mat[0], t_mat[1], t_mat[2], t_mat[4], t_mat[ 5], t_mat[ 6], t_mat[ 8], t_mat[ 9], t_mat[10]) * det);
-		
+
 			#undef MDET3
 		}
-		
+
 		inline const Mat4 TransposeMat4(const Mat4& t_mat)
 		{
 			return Mat4(t_mat[ 0], t_mat[ 4], t_mat[ 8], t_mat[12],
@@ -250,11 +250,11 @@ namespace Fregat
 			     - t_mat[1] * (t_mat[3] * t_mat[8] - t_mat[5] * t_mat[6])
 			     + t_mat[2] * (t_mat[3] * t_mat[7] - t_mat[4] * t_mat[6]);
 		}
-		
+
 		inline const Mat3 InverseMat3(const Mat3& t_mat)
 		{
 			const float det = 1.0f / DeterminantMat3(t_mat);
-		
+
 			return Mat3((t_mat[4] * t_mat[8] - t_mat[5] * t_mat[7]) * det,
 			            (t_mat[2] * t_mat[7] - t_mat[1] * t_mat[8]) * det,
 			            (t_mat[1] * t_mat[5] - t_mat[2] * t_mat[4]) * det,
@@ -265,7 +265,7 @@ namespace Fregat
 			            (t_mat[1] * t_mat[6] - t_mat[0] * t_mat[7]) * det,
 			            (t_mat[0] * t_mat[4] - t_mat[1] * t_mat[3]) * det);
 		}
-		
+
 		inline const Mat3 TransposeMat3(const Mat3& t_mat)
 		{
 			return Mat3(t_mat[0], t_mat[3], t_mat[6],
@@ -273,13 +273,13 @@ namespace Fregat
 			            t_mat[2], t_mat[5], t_mat[8]);
 		}
 
-		inline const Mat4 Orthographic(float t_left, float t_right, float t_bottom, 
+		inline const Mat4 Orthographic(float t_left, float t_right, float t_bottom,
 									   float t_top, float t_zNear, float t_zFar)
 		{
 			const float tx = - (t_right + t_left) / (t_right - t_left),
 			            ty = - (t_top + t_bottom) / (t_top - t_bottom),
 			            tz = - (t_zFar + t_zNear) / (t_zFar - t_zNear);
-		
+
 			return Mat4(2 / (t_right - t_left), 0, 0, tx,
 			            0, 2 / (t_top - t_bottom), 0, ty,
 			            0, 0, -2 / (t_zFar - t_zNear), tz,
@@ -298,7 +298,7 @@ namespace Fregat
 						0, 0, -1, 0);
 		}
 
-		inline const Mat4 Frustum(float t_left, float t_right, float t_bottom, 
+		inline const Mat4 Frustum(float t_left, float t_right, float t_bottom,
 									float t_top, float t_zNear, float t_zFar)
 		{
 			return Mat4((2 * t_zNear) / (t_right - t_left), 0, (t_right + t_left) / (t_right - t_left), 0,
@@ -328,7 +328,7 @@ namespace Fregat
 			const float cx = cosf(t_vec.x * math_radians), sx = sinf(t_vec.x * math_radians),
 					cy = cosf(t_vec.y * math_radians), sy = sinf(t_vec.y * math_radians),
 					cz = cosf(t_vec.x * math_radians), sz = sinf(t_vec.z * math_radians);
-		
+
 			// rotationX * rotationY * rotationZ
 			return Mat4(cy * cz, -cy * sz, sy, 0,
 						cx * sz + sx * cz * sy, cx * cz - sx * sy * sz, -cy * sx, 0,
@@ -342,7 +342,7 @@ namespace Fregat
 		}
 
 		inline float Length(const Vec2& t_vec)
-		{ 
+		{
 			return sqrtf(Dot(t_vec, t_vec));
 		}
 
@@ -351,15 +351,15 @@ namespace Fregat
 			return Length(t_v1 - t_v2);
 		}
 
-		inline Vec2& Normalize(const Vec2& t_vec)
+		inline Vec2 Normalize(const Vec2& t_vec)
 		{
 			return Vec2(t_vec / Length(t_vec));
 		}
 
 		inline Vec3 Cross(const Vec3& t_vec1, const Vec3& t_vec2)
 		{
-			return Vec3(t_vec1.y * t_vec2.z - t_vec1.z * t_vec2.y, 
-						t_vec1.z * t_vec2.x - t_vec1.x * t_vec2.z, 
+			return Vec3(t_vec1.y * t_vec2.z - t_vec1.z * t_vec2.y,
+						t_vec1.z * t_vec2.x - t_vec1.x * t_vec2.z,
 						t_vec1.x * t_vec2.y - t_vec1.y * t_vec2.x);
 		}
 
@@ -378,7 +378,7 @@ namespace Fregat
 			return Length(t_vec1 - t_vec2);
 		}
 
-		inline Vec3& Normalize(const Vec3 t_vec)
+		inline Vec3 Normalize(const Vec3 t_vec)
 		{
 			return Vec3(t_vec / Length(t_vec));
 		}
@@ -388,7 +388,7 @@ namespace Fregat
 			const Vec3 f = Normalize(t_position - t_center);
 			const Vec3 s = Normalize(Cross(t_up, f));
 			const Vec3 u = Normalize(Cross(f, s));
-		
+
 			return Mat4(s.x, s.y, s.z, 0,
 			            u.x, u.y, u.z, 0,
 			            f.x, f.y, f.z, 0,
@@ -415,6 +415,6 @@ namespace Fregat
 		//inline float Length(const Vec4 &v);
 		//inline float Distance(const Vec4 &v1, const Vec4 &v2);
 		//inline Vec4 Normalize(Vec4);
-		
+
 	}
 }
